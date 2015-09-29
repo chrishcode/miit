2b616c8cb82a9444b2a711adf0a38afd12b1225e
Christopher Wohlfarth <chris.wohlfarth@gmail.com>
2015-09-29 15:45:23 +0200

    Started styling template

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Miit.io</title>
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css') }}">
+	<link href="{{ URL::asset('https://fonts.googleapis.com/css?family=Comfortaa:400,300,700') }}" rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
+</head>
+<body>
+	<nav id="navbar" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="logo navbar-brand" href="{{ url('/') }}"><img class="logoicon" alt="logo" src="img/logoicon.png">Miit.io</a>
+            </div>
+          
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="{{ url('/register') }}">Register</a>
+                </li>
+                <li>
+                    <a href="{{ url('/login') }}">Login</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+	<div class="container">
+		@yield('content')
+	</div>
+
+<!-- 	<div id="footer">
+		<div class="container">
+	        <ul class="footermenu">
+	            <li>
+	                &copy; miit.io</a>
+	            </li>
+	            <li>
+	                <a href="{{ url('/about') }}">About</a>
+	            </li>
+	            <li>
+	                <a href="{{ url('/contact') }}">Contact</a>
+	            </li>
+	        </ul>
+	    </div>
+	</div> -->
+
+    <div id="footer" class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container">
+                <ul class="nav navbar-nav footermenu">
+			        <li>
+		                &copy; miit.io</a>
+		            </li>
+		            <li>
+		                <a href="{{ url('/about') }}">About</a>
+		            </li>
+		            <li>
+		                <a href="{{ url('/contact') }}">Contact</a>
+		            </li>
+                </ul>
+        </div>
+    </div>
+
+</body>
+</html>
