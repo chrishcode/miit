0d094036da2ae8b377bb14ecef383873ea86143a
erikwaahlstrom <erik93_93@hotmail.com>
2015-10-07 11:39:00 +0200

    datum form
    datum form

--- resources/views/create_meeting.blade.php
+++ resources/views/create_meeting.blade.php
@@ -3,34 +3,44 @@
 @section('content')
 
 <div id="content" class="container">
-<form id="Meetingform">
+<form action="" method="post" id="Meetingform" >
+
+<h2 id="newmeeting">New meeting</h2>
 
   <div class="form-group">
-    <label for="exampleInputEmail1">Name</label>
+    <label for="name">Name</label>
     <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Name">
   </div>
 
   <div class="form-group">
-    <label for="exampleInputPassword1">Email</label>
+    <label for="email">Email</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Email">
   </div>
 
   <div class="form-group">
-    <label for="exampleInputPassword1">Description</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Description">
-  </div>
+  <label for="description">Description</label>
+  <textarea class="form-control" rows="5" id="description" placeholder="Description"></textarea>
+</div>
 
-  <div class="form-group">
+  <div class="form-group" id="timeinput">
   <label for="exampleInputEmail1">Add dates</label>
 	<input id="datetimepicker" type="text" class="form-control" placeholder="Add dates">
 	</div>
 
+	<a id="btn2" href="#">Add</a>
+
 	 <div class="form-group">
-    <label for="exampleInputPassword1">Email/Invite</label>
+    <label for="email/invite">Email/Invite</label>
     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Email/Invite">
   </div>
  
   <button type="submit" class="btn btn-success">New meeting</button>
+
+  <fieldset>
+        <legend>Inputs</legend>
+            <div id="extender"></div>
+            <p><a href="#" id="add">Add</a></p>
+    </fieldset>
 
 </form>
 </div>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,6 +12,8 @@
 
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('jquery.datetimepicker.css') }}">
 
+	<link rel="stylesheet" href="{{ URL::asset('http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css') }}" />
+
 	<link href="{{ URL::asset('https://fonts.googleapis.com/css?family=Comfortaa:400,300,700') }}" rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
@@ -50,13 +52,42 @@
 	</div>
 </div> 
 
-</body>
 
 <script src="{{ URL::asset('jquery.js') }}"></script>
 <script src="{{ URL::asset('build/jquery.datetimepicker.full.min.js') }}"></script>
+
 <script>jQuery('#datetimepicker').datetimepicker({
 	format:'d-m-Y H:i'
 });
 </script>
+
+<script type="text/javascript">
+$(function() {
+
+$('.remove').live('click', function() {
+        $(this).parent().fadeOut(300, function(){ 
+            $(this).empty();
+            return false;
+        });
+    });
+
+  var options = '<p><input id="datetimepicker" type="text" class="datepicker" name="dates[]" value="" size="10" placeholder="Add dates"/>   <a href="#" class="remove">Remove</a></p>'; 
+
+$('a#add').click(function() {
+        $(options).fadeIn("slow").appendTo('#extender'); 
+        i++;    
+        return false;
+    });
+});
+
+// $(document).ready(function(){
+
+//     $("#btn2").click(function(){
+//         $("#timeinput").append("<input id='datetimepicker' type='text' class='form-control' placeholder='Add dates'>");
+//     });
+// });
+</script>
+
+</body>
 
 </html>
