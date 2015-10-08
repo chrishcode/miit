8b62a930cec5b2e05a25a2855b73762fd525ff4f
Petterromhagen <petter.romhagen@hotmail.com>
2015-10-08 15:44:19 +0200

    Current meetings
    meetings on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,28 +10,42 @@
 <form id="dashboardform">
 
   	<div class="form-group">
-  			<img class="edit-name" alt="user" src="img/edit-name.png">
+  			<img class="edit-name" alt="User" src="img/edit-name.png">
     		<input type="name" class="form-control" id="dashboardform" placeholder="Name">
  	</div>
 
 	  <div class="form-group">
-	  		<img class="edit-name" alt="user" src="img/edit-email.png">
+	  		<img class="edit-name" alt="Email" src="img/edit-email.png">
 	    	<input type="email" class="form-control" id="dashboardform" placeholder="Email">
 	  </div>
 
 	  <div class="form-group">	
-	  		<img class="edit-name" alt="user" src="img/edit-password.png">
+	  		<img class="edit-name" alt="Password"sword src="img/edit-password.png">
 	    	<input type="password" class="form-control" id="dashboardform" placeholder="Password">
 	  </div>
- 	
+ 		
   		<button type="submit" id="button-dashboard" class="btn btn-success">Save</button>
   	
 </form>
 
-<h2 id="dashboard-meetings">Meetings</h2>
+	<h2 id="dashboard-meetings">Meetings</h2>
 
 <hr class="hr">
 
+	<h3 id="meet1">Jesper Hakeröd</h3>
+		<img class="numb1" alt="1" src="img/numb1.png">
+		<img class="mrgreen" alt="1" src="img/green.png">
+	<p id="meet2">Handledning</p>
+
+	<h3 id="meet1">Martin Gustafsson</h3>
+		<img class="numb1" alt="1" src="img/numb1.png">
+		<img class="mrgreen" alt="1" src="img/red.png">
+	<p id="meet2">Kör inte de tricket..</p>
+
+	<h3 id="meet1">Jonas Nilsson</h3>
+		<img class="numb1" alt="1" src="img/numb1.png">
+		<img class="mrgreen" alt="1" src="img/yellow.png">	
+	<p id="meet2">Lite käbbel om...</p>
 
 </div>
 
